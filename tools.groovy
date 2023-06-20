41994c4028654774a3d26938d0bc137995f7fbb1
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-06-20 12:34:32 +0530

    Add files via upload

---
+++ tools.groovy
@@ -0,0 +1,36 @@
+// Path to tools used by the pipeline
+Activate="/opt/data/home/varsha/mambaforge/bin/activate"
+bpipe="/opt/data/home/varsha/mambaforge/bin/bpipe"
+hisat2="/opt/data/home/varsha/mambaforge/bin/hisat2"
+stringtie="/opt/data/home/varsha/mambaforge/bin/stringtie"
+gffread="/opt/data/home/varsha/mambaforge/bin/gffread"
+gffcompare="/opt/data/home/varsha/mambaforge/bin/gffcompare"
+pblat="/opt/data/home/varsha/mambaforge/bin/pblat"
+samtools="/opt/data/home/varsha/mambaforge/bin/samtools"
+bowtie2="/usr/local/bin/bowtie2"
+bamToFastq="/opt/data/home/varsha/mambaforge/bin/bamToFastq"
+fastp="/opt/data/home/varsha/mambaforge/bin/fastp"
+seqtk="/opt/data/home/varsha/mambaforge/bin/seqtk"
+python3="/opt/data/home/varsha/mambaforge/bin/python3"
+
+// Path to PLEK
+PLEK="/opt/data/home/varsha/mambaforge/bin/PLEK.py"
+PLEKModelling="/opt/data/home/varsha/mambaforge/bin/PLEKModelling.py"
+
+// Path to rnasamba
+rnasamba="/opt/data/home/varsha/mambaforge/envs/rnasamba/bin/rnasamba"
+
+// Path to FEELnc env and tools used by the pipeline
+FEELnc="/opt/data/home/varsha/FEELnc"
+perl="/opt/data/home/varsha/FEELnc/bin/perl"
+FEELnc_filter="/opt/data/home/varsha/FEELnc/bin/FEELnc_filter.pl"
+FEELnc_codpot="/opt/data/home/varsha/FEELnc/bin/FEELnc_codpot.pl"
+FEELnc_classifier="/opt/data/home/varsha/FEELnc/bin/FEELnc_classifier.pl"
+
+// Path to python 2.7, CPC2, CPAT and slncky
+python2="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/python"
+cpc2="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/CPC2.py"
+make_hexamer="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/make_hexamer_tab.py"
+logit_model="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/make_logitModel.py"
+CPAT="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/cpat.py"
+slncky="/opt/data/home/varsha/mambaforge/envs/cpc2-cpat-slncky/bin/slncky"
