a296bed47aaf1236233904b0cca5127d5afdf716
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-07-21 16:41:10 +0530

    Update fastp.groovy

--- stages/fastp.groovy
+++ stages/fastp.groovy
@@ -8,20 +8,22 @@
 fastp_dir="fastp"
 
 FastP = {
-	def input_fatsq_options = ""
-	if(reads_R2=="")
-	input_fatsq_options = "-i "+input
-	else
-	input_fastq_options = "-i "+input1+" -I "+input2
-	output.dir=fastp_dir
-	if(reads_R2=="")
-	produce(branch.name+".fastq.gz",branch.name+".html",branch.name+".json"){
-        exec "$fastp --thread $threads $input_fatsq_options -o $output1 --html $output2 --json $output3"
-	}
-        else
-        produce(branch.name+"_1.fastq.gz", branch.name+"_2.fastq.gz",branch.name+".html",branch.name+".json"){
-        exec "$fastp --thread $threads $input_fatsq_options -o $output1 -O output2 --html $output3 --json $output4"
-	}
+    def input_fastq_options = ""
+    if (reads_R2 == "") {
+        input_fastq_options = "-i " + input
+    } else {
+        input_fastq_options = "-i " + input1 + " -I " + input2
+    }
+    output.dir = fastp_dir
+    if (reads_R2 == "") {
+        produce(branch.name + ".fastq.gz", branch.name + ".html", branch.name + ".json") {
+            exec "$fastp --thread $threads $input_fastq_options -o $output1 --html $output2 --json $output3"
+        }
+    } else {
+        produce(branch.name + "_1.fastq.gz", branch.name + "_2.fastq.gz", branch.name + ".html", branch.name + ".json") {
+            exec "$fastp --thread $threads $input_fastq_options -o $output1 -O $output2 --html $output3 --json $output4"
+        }
+    }
 }
 
 quality_trimming = segment {fastqInputFormat * [FastP] }
