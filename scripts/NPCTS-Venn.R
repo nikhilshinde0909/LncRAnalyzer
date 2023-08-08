33ddd3b66461641428c08052b5d809c1bea6600b
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-08-08 14:37:06 +0530

    Update NPCTS-Venn.R

--- scripts/NPCTS-Venn.R
+++ scripts/NPCTS-Venn.R
@@ -7,8 +7,8 @@ conflict_prefer("lag", "dplyr")
 
 # Get command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-if (length(args) < 4) {
-  stop("Usage: NPCTS-venn.R CPAT_list CPC2_list RNAsamba_list output_file")
+if (length(args) < 3) {
+  stop("Usage: NPCTS-venn.R CPAT_list CPC2_list RNAsamba_list")
 }
 
 # Read data from input files
@@ -16,7 +16,7 @@ CPAT <- read.table(args[1], header = FALSE, sep = '\t')
 CPC2 <- read.table(args[2], header = FALSE, sep = '\t')
 RNAsamba <- read.table(args[3], header = FALSE, sep = '\t')
 
-# colors
+# Colors 
 myCol <- brewer.pal(8, "Accent")
 
 # Venn
@@ -24,7 +24,7 @@ data1 <- list('CPAT' = CPAT$V1,
               'CPC2' =  CPC2$V1,
               'RNAsamba'=RNAsamba$V1)
 
-tiff(args[4], units="cm", width = 15,
+tiff("LncRAnalyzer-summary/LncRAnalyzer-NPCTs-Venn.tiff", units="cm", width = 15,
      height=12, res=300)
 venn(data1, ilcs = 1.1, sncs = 1.3, ilabels = TRUE, ellipse = TRUE, opacity = 0.30, ggplot = TRUE, box = FALSE, 
      zcolor = myCol, cex = 0.8)
