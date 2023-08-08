e4a2984004881abff7e993b5fb3c53a544dede16
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-08-08 11:37:00 +0530

    Update Lnc-Venn.R

--- scripts/Lnc-Venn.R
+++ scripts/Lnc-Venn.R
@@ -5,10 +5,8 @@ library(RColorBrewer)
 # Get command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 5) {
-  stop("Usage: Lnc-venn.R CPAT_list CPC2_list RNAsamba_list FEELnc_list out_dir")
+  stop("Usage: Lnc-venn.R CPAT_list CPC2_list RNAsamba_list FEELnc_list output_file")
 }
-
-setwd(args[5])
 
 # Read data from input files
 CPAT <- read.table(args[1], header = FALSE, sep = '\t')
@@ -22,7 +20,7 @@ data1 <- list('FEELnc'=  FEELnc$V1,
               'CPC2' =  CPC2$V1,
               'RNAsamba'=RNAsamba$V1)
 
-tiff("LncRAnalyzer-lnc_venn.tiff", units="cm", width = 15,
+tiff(args[1], units="cm", width = 15,
      height=12, res=300)
 venn(data1, ilcs = 1.1, sncs = 1.3, ilabels = TRUE, ellipse = TRUE, opacity = 0.30, ggplot = TRUE, box = FALSE, 
      zcolor = myCol, cex = 0.8)
