ba5aa196e82fa52dfe4e306b37c49a2879742334
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-08-13 20:41:21 +0530

    Update rRNA_unmapped.groovy

--- stages/rRNA_unmapped.groovy
+++ stages/rRNA_unmapped.groovy
@@ -8,6 +8,7 @@
 unmapped_reads_dir="unmapped_reads"
 
 //User specified read alignment to rRNAs
+
 build_rRNA_index = {
 	output.dir=unmapped_reads_dir
 	produce("rRNA.1.ht2"){
@@ -15,36 +16,28 @@ build_rRNA_index = {
         }
 }
 
-map_reads_to_rRNAs = {
-        def input_reads_option=""
-	if(reads_R2=="")
-             input_reads_option = "-U "+ input
-        else
-             input_reads_option = "-1 "+ input1 + " -2 "+ input2
-        doc "Aligning reads to rRNAs using HISAT2"
-	output.dir=unmapped_reads_dir
-	produce(branch.name+".rRNA.bam",branch.name+".rRNA.summary"){
-        exec "$hisat2 $hisat2_options --summary-file $output2 -x $input.ht2.prefix.prefix $input_reads_option | $samtools view -Su - | $samtools sort - -o $output1"
-        }
-}
-
-unmapped_bam = {
-        def bam_options=""
-        if(reads_R2=="")
-                bam_options=unmapped_bam_options
-        else
-            	bam_options=unmapped_bam_paired_options
-        output.dir=unmapped_reads_dir
-        produce(branch.name+".u.bam"){
-	exec "$samtools view -@ $threads $bam_options $input.rRNA.bam -o $output && rm $input.rRNA.bam"
-        }
-}
-
-qsorted_bam = {
-        output.dir=unmapped_reads_dir
-        produce(branch.name+".qsort.bam"){
-	exec "$samtools sort -@ $threads -n $input.u.bam -o $output && rm $input.u.bam"
-	}
+map_reads_to_rRNAs_and_unmapped_bam = {
+    def input_reads_option = ""
+    def bam_options = ""
+    
+    if (reads_R2 == "") {
+        input_reads_option = "-U ${input}"
+        bam_options = unmapped_bam_options
+    } else {
+        input_reads_option = "-1 ${input1} -2 ${input2}"
+        bam_options = unmapped_bam_paired_options
+    }
+    
+    doc "Aligning reads to rRNAs using HISAT2"
+    output.dir = unmapped_reads_dir
+    
+    produce("${branch.name}.rRNA.u.bam", "${branch.name}.rRNA.summary") {
+        exec """
+        $hisat2 $hisat2_options --summary-file $output2 -x $input.ht2.prefix.prefix $input_reads_option |
+        $samtools view $bam_options -Su - |
+        $samtools sort -n - -o $output1
+        """
+    }
 }
 
 unmapped_reads = {
@@ -52,13 +45,13 @@ unmapped_reads = {
         if(reads_R2=="")
        	produce(branch.name+".fastq"){
         exec """
-        $bamToFastq -i $input.qsort.bam-fq $output && rm $input.qsort.bam       
+        $bamToFastq -i $input.rRNA.u.bam -fq $output && rm $input.rRNA.u.bam       
         """
 	}
 	else
 	produce(branch.name+"_1.fastq",branch.name+"_2.fastq"){
         exec """
-        $bamToFastq -i $input.qsort.bam -fq $output1 -fq2 $output2 && rm $input.qsort.bam
+        $bamToFastq -i $input.rRNA.u.bam -fq $output1 -fq2 $output2 && rm $input.rRNA.u.bam
         """
 	}
 }
@@ -73,5 +66,5 @@ gzip_reads = {
     exec "gzip $input_gzip_options"
 }
 
-unmapped_reads_to_rRNAs = segment { build_rRNA_index + fastqInputFormat * [ map_reads_to_rRNAs + unmapped_bam ]  +
-                        fastqInputFormat * [ qsorted_bam , unmapped_reads + gzip_reads ] }
+unmapped_reads_to_rRNAs = segment { build_rRNA_index + fastqInputFormat * [ map_reads_to_rRNAs_and_unmapped_bam ]  +
+                        fastqInputFormat * [ unmapped_reads + gzip_reads ] }
