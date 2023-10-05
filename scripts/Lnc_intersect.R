7fb1f086d068804297f5a1fc740750bca13f2e10
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-10-05 11:16:15 +0530

    Create Lnc_intersect.R

---
+++ scripts/Lnc_intersect.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(tidyverse)
+
+# Check if the required number of arguments is provided
+if (length(commandArgs(trailingOnly = TRUE)) != 4) {
+  cat("Usage: Rscript script.R FEELnc_file CPAT_file CPC2_file RNAsamba_file\n")
+  quit(save = "no", status = 1)
+}
+
+# Get input file paths from command-line arguments
+FEELnc_file <- commandArgs(trailingOnly = TRUE)[1]
+CPAT_file <- commandArgs(trailingOnly = TRUE)[2]
+CPC2_file <- commandArgs(trailingOnly = TRUE)[3]
+RNAsamba_file <- commandArgs(trailingOnly = TRUE)[4]
+
+# Read data from input files
+FEELnc <- read.table(FEELnc_file, header = FALSE, sep = '\t')
+CPAT <- read.table(CPAT_file, header = FALSE, sep = '\t')
+CPC2 <- read.table(CPC2_file, header = FALSE, sep = '\t')
+RNAsamba <- read.table(RNAsamba_file, sep = '\t', header = FALSE)
+
+# Combine data using inner joins
+data <- list(FEELnc, CPAT, CPC2, RNAsamba) %>%
+  reduce(inner_join)
+
+# Remove duplicates based on column V1
+data <- data[!duplicated(data$V1),]
+
+# Write the result to an output file
+output_file <- 'LncRAnalyzer-Lncs-intersect.txt'
+write.table(data, output_file, row.names = FALSE, col.names = FALSE, sep = '\t', quote = FALSE)
