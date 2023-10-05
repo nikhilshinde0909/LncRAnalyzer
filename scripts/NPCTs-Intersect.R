3729c6087596356d89881a07320ad54f796bab2d
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2023-10-05 12:12:30 +0530

    Update NPCTs-Intersect.R

--- scripts/NPCTs-Intersect.R
+++ scripts/NPCTs-Intersect.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(tidyverse)
 
 # Check if the required number of arguments is provided
-if (length(commandArgs(trailingOnly = TRUE)) != 3) {
-  cat("Usage: Rscript NPCTs-Intersect.R CPAT_file CPC2_file RNAsamba_file\n")
+if (length(commandArgs(trailingOnly = TRUE)) != 4) {
+  cat("Usage: Rscript NPCTs-Intersect.R CPAT_file CPC2_file RNAsamba_file output_file\n")
   quit(save = "no", status = 1)
 }
 
@@ -11,6 +11,7 @@ if (length(commandArgs(trailingOnly = TRUE)) != 3) {
 CPAT_file <- commandArgs(trailingOnly = TRUE)[1]
 CPC2_file <- commandArgs(trailingOnly = TRUE)[2]
 RNAsamba_file <- commandArgs(trailingOnly = TRUE)[3]
+output_file <- commandArgs(trailingOnly = TRUE)[4]
 
 # Read data from input files
 CPAT <- read.table(CPAT_file, header = FALSE, sep = '\t')
@@ -25,5 +26,4 @@ data <- list(FEELnc, CPAT, CPC2, RNAsamba) %>%
 data <- data[!duplicated(data$V1),]
 
 # Write the result to an output file
-output_file <- 'LncRAnalyzer-NPCTs-intersect.txt'
 write.table(data, output_file, row.names = FALSE, col.names = FALSE, sep = '\t', quote = FALSE)
