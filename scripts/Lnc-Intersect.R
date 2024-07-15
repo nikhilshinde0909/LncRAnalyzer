67aa9510308c3054f7dbdd50dd4b249fbbaa310f
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-07-15 18:42:57 +0530

    Update Lnc-Intersect.R

--- scripts/Lnc-Intersect.R
+++ scripts/Lnc-Intersect.R
@@ -4,8 +4,8 @@ library(dplyr)
 library(tidyverse)
 
 # Check if the required number of arguments is provided
-if (length(commandArgs(trailingOnly = TRUE)) != 5) {
-  cat("Usage: Rscript Lnc_Intersect.R FEELnc_file CPAT_file CPC2_file RNAsamba_file output_file\n")
+if (length(commandArgs(trailingOnly = TRUE)) != 6) {
+  cat("Usage: Rscript Lnc_Intersect.R FEELnc_file CPAT_file CPC2_file RNAsamba_file LGC_file output_file\n")
   quit(save = "no", status = 1)
 }
 
@@ -14,16 +14,18 @@ FEELnc_file <- commandArgs(trailingOnly = TRUE)[1]
 CPAT_file <- commandArgs(trailingOnly = TRUE)[2]
 CPC2_file <- commandArgs(trailingOnly = TRUE)[3]
 RNAsamba_file <- commandArgs(trailingOnly = TRUE)[4]
-output_file <- commandArgs(trailingOnly = TRUE)[5] 
+LGC_file <- commandArgs(trailingOnly = TRUE)[5]
+output_file <- commandArgs(trailingOnly = TRUE)[6] 
 
 # Read data from input files
 FEELnc <- read.table(FEELnc_file, header = FALSE, sep = '\t')
 CPAT <- read.table(CPAT_file, header = FALSE, sep = '\t')
 CPC2 <- read.table(CPC2_file, header = FALSE, sep = '\t')
 RNAsamba <- read.table(RNAsamba_file, sep = '\t', header = FALSE)
+LGC <- read.table(LGC_file, sep = '\t', header = FALSE)
 
 # Combine data using inner joins
-data <- list(FEELnc, CPAT, CPC2, RNAsamba) %>%
+data <- list(FEELnc, CPAT, CPC2, RNAsamba, LGC) %>%
   reduce(inner_join)
 
 # Remove duplicates based on column V1
