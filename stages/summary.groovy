89605493661e563a2d2dbdd4c7509c1faf97d484
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-08-30 19:56:45 +0530

    Update summary.groovy

--- stages/summary.groovy
+++ stages/summary.groovy
@@ -1,5 +1,5 @@
 /****************************************************************
- ** Stages to retieve quality trimming and removing rRNA reads
+ ** Stages to get summary and intersection
  ** Author: Nikhil Shinde <sd1172@srmist.edu.in>
  ** Last Update: 30/05/2023
  ****************************************************************/
@@ -16,36 +16,36 @@ get_FEELnc_results = {
 
 lnc_venn = {
 	output.dir=summary_dir
-	from("final_lnc_RNAs-CPAT.list","final_lnc_RNAs-cpc2.list","final_lnc_RNAs-rnasamba.list","FEELnc_out-lnc.list","final_lnc_RNAs-lgc.list") produce("LncRAnalyzer-lnc_venn.log") {
-	exec "$Rscript $lnc_venn_script $input1 $input2 $input3 $input4 $input5 > $output"
+	from("final_lnc_RNAs-CPAT.list","final_lnc_RNAs-cpc2.list","final_lnc_RNAs-rnasamba.list","FEELnc_out-lnc.list","final_lnc_RNAs-lgc.list","final_lncRNAs_pfamscan.list") produce("LncRAnalyzer-lnc_venn.log") {
+	exec "$Rscript $lnc_venn_script $input1 $input2 $input3 $input4 $input5 $input6 > $output"
 	}
 } 
 
 npcts_venn = {
 	output.dir=summary_dir
-	from("final_NPCTs-CPAT.list","final_NPCTs-cpc2.list","final_NPCTs-rnasamba.list","final_NPCTs-lgc.list") produce("LncRAnalyzer-NPCTs-Venn.log") {
-	exec "$Rscript $npcts_venn_script $input1 $input2 $input3 $input4 > $output"
+	from("final_NPCTs-CPAT.list","final_NPCTs-cpc2.list","final_NPCTs-rnasamba.list","final_NPCTs-lgc.list","final_NPCTs_pfamscan.list") produce("LncRAnalyzer-NPCTs-Venn.log") {
+	exec "$Rscript $npcts_venn_script $input1 $input2 $input3 $input4 $input5 > $output"
 	}
 }
 
 lnc_intersect = {
 	output.dir=summary_dir
-	from("final_lnc_RNAs-CPAT.list","final_lnc_RNAs-cpc2.list","final_lnc_RNAs-rnasamba.list","FEELnc_out-lnc.list","final_lnc_RNAs-lgc.list") produce("LncRAnalyzer-Lncs-intersect.txt") {
-	exec "$Rscript $lnc_intersect_script $input1 $input2 $input3 $input4 $input5 $output"
+	from("final_lnc_RNAs-CPAT.list","final_lnc_RNAs-cpc2.list","final_lnc_RNAs-rnasamba.list","FEELnc_out-lnc.list","final_lnc_RNAs-lgc.list","final_lncRNAs_pfamscan.list") produce("LncRAnalyzer-Lncs-intersect.txt") {
+	exec "$Rscript $lnc_intersect_script $input1 $input2 $input3 $input4 $input5 $input6 $output"
 	}
 } 
 
 npcts_intersect = {
 	output.dir=summary_dir
-	from("final_NPCTs-CPAT.list","final_NPCTs-cpc2.list","final_NPCTs-rnasamba.list","final_NPCTs-lgc.list") produce("LncRAnalyzer-NPCTs-intersect.txt") {
-	exec "$Rscript $npct_intersect_script $input1 $input2 $input3 $input4 $output"
+	from("final_NPCTs-CPAT.list","final_NPCTs-cpc2.list","final_NPCTs-rnasamba.list","final_NPCTs-lgc.list","final_NPCTs_pfamscan.list") produce("LncRAnalyzer-NPCTs-intersect.txt") {
+	exec "$Rscript $npct_intersect_script $input1 $input2 $input3 $input4 $input5 $output"
 	}
 }
 
 final_lncs_gtf = {
 	output.dir=summary_dir
 	from("LncRAnalyzer-Lncs-intersect.txt") produce("LncRAnalyzer-Lncs-intersect.gtf") {
-	exec "cat ${intergenic_dir}/feelnc_intergenic.codpot.lncRNA.gtf ${shuffle_dir}/feelnc_shuffle.codpot.lncRNA.gtf | grep -Ff $input|sort -u > $output"
+	exec "cat ${intergenic_dir}/feelnc_intergenic.codpot.lncRNA.gtf ${shuffle_dir}/feelnc_shuffle.codpot.lncRNA.gtf | sort -u | grep -w -f $input > $output"
 	}
 }
 
