7e09e191bb2ed73eab06911302dc1d79c40724de
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-09-03 17:27:39 +0530

    Update lnc_npc_transcript_filter.groovy

--- stages/lnc_npc_transcript_filter.groovy
+++ stages/lnc_npc_transcript_filter.groovy
@@ -11,17 +11,14 @@ lnc_NPCTs_dir="Putative_lnc-NPCTs"
 putative_lnc_npc_transcripts_list = {
       output.dir=lnc_NPCTs_dir
       from("gffcompare.annotated.classcode_selected_lnc-npcts.gtf") produce("Putative.lnc-NPCTs.list"){
-        exec "grep -E -w -v 'cmp_ref' $input |cut -d ';' -f 1|cut -f 9|sed 's/transcript_id //g;s/\"//g' > $output"
+        exec "cut -d ';' -f 1 $input|cut -f 9|sed 's/transcript_id //g;s/\"//g' > $output"
       }
 }
 
 putative_lnc_NPCTs = {
       output.dir=lnc_NPCTs_dir
-      from("gffcompare.annotated.classcode_selected_lnc-npcts.gtf") produce("Putative.lnc_NPCTs.gtf","Putative.lnc_NPCTs.fa"){
-        exec """
-	grep -E -w -v 'cmp_ref' $input > $output1 ;
-	${gffread} $output1 -g $genome -w $output2
-	"""
+      from("gffcompare.annotated.classcode_selected_lnc-npcts.gtf") produce("Putative.lnc_NPCTs.fa"){
+        exec "${gffread} $input-g $genome -w $output"
       }
 }
 
