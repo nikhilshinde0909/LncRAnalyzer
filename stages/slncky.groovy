67c250e766b277ffc5cbb4d7b145bdc21df8aebf
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-10-11 14:29:09 +0530

    Update slncky.groovy

--- stages/slncky.groovy
+++ stages/slncky.groovy
@@ -82,7 +82,7 @@ run_slncky = {
 	from("annotation.config","Putative-lnc-nptcs.bed") produce("slncky_out.lncs.info.txt"){
 	exec """
 	source $Activate cpc2-cpat-slncky ;
-	$slncky -n $threads -c $input1 $input2 $org_name $slncky_options $output.prefix.prefix.prefix
+	$python2 $slncky -n $threads -c $input1 $input2 $org_name $slncky_options $output.prefix.prefix.prefix
 	"""
 	  }
 }
@@ -93,7 +93,7 @@ ortholog_search = {
 	from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name+".orthologs.top.txt"){
         exec """
         source ${Activate} cpc2-cpat-slncky ;
-        $slncky -n $threads -c $input1 $input2 $slncky_ortho_options $org_name $output.prefix.prefix.prefix
+        $python2 $slncky -n $threads -c $input1 $input2 $slncky_ortho_options $org_name $output.prefix.prefix.prefix
         """
     	} 
 	} else {
