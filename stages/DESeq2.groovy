f6d3de8df9fb148decda93ecd90ba7bc3813c622
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-12-07 12:52:53 +0530

    Update DESeq2.groovy

--- stages/DESeq2.groovy
+++ stages/DESeq2.groovy
@@ -3,10 +3,10 @@
  ** Last Update: 22/11/2024
  *********************************************************/
 
-DESeq2_dir="LncRAnalyzer-summary"
+// Perform differential expression analysis
 
 perform_deseq2 = {
-	output.dir=DESeq2_dir
+	output.dir=summary_dir
 	if(design !=""){
 	from(org_name+"_PCG.TSV",org_name+"_Lnc.TSV") produce(org_name+"_PCG_DESeq2.TSV",org_name+"_Lnc_DESeq2.TSV"){
 	exec """
